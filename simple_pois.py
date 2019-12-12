3370ab63ccb13072046aaa7dca8bbc640bce2dec
JackStelling <57093348+JackStelling@users.noreply.github.com>
2019-12-12 14:41:03 +0000

    Add in the enter IP address feature

--- simple_pois.py
+++ simple_pois.py
@@ -11,11 +11,14 @@ def load_poisson(URL,lam,k):
     for x in i_r_t:
         time.sleep(x)
         requests.get(URL)
-        #print(x)
+        print(x)
 
 
-URL = input("Enter URL e.g http://<AWScloud I.P>:8080/primecheck   ")
+IP = input("Enter the IP  ")
 lam = float( input("Enter Lambda (rate) value   ") )
 k = int ( input("Enter the number of times to call the URL   ") )
+
+URL="http://192.168.99.100:8080/primecheck"
+URL=URL[:7] +IP+ URL[21:]
 
 load_poisson(URL,lam,k)
