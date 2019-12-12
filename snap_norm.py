653954164acd6cd3aab8e059033483cfaa12c703
JackStelling <57093348+JackStelling@users.noreply.github.com>
2019-12-12 15:30:04 +0000

    Cloud compatible.
    When deployed on the cloud the IP address changes each time an instance is created. Added in the user input IP address function. Meaning the function can work in any domain as long as the containers are present

--- snap_norm.py
+++ snap_norm.py
@@ -1,17 +1,15 @@
-import numpy as np
-import requests
-import time
+def snap_normal(URL, mean, sd, k):
 
-
-def snap_norm(URL, mean, sd, k):
 
     i=0
     i_r_t=abs(np.random.normal(mean,sd,k))
     # limitation becuase of negative times.
     tic=time.perf_counter()
     requests.get(URL)
+
     global data1
     global data2
+
     data_norm_Tom1=[None]*10
     data_norm_Tom2=[None]*10
 
@@ -24,8 +22,8 @@ def snap_norm(URL, mean, sd, k):
         if (toc-tic >= 90):
             tic=time.perf_counter()
             i=i+1
-            JSON3_norm = requests.get("http://192.168.99.100:3000/api/v1.3/docker/jack_Tomcat.1")
-            JSON4_norm = requests.get("http://192.168.99.100:3000/api/v1.3/docker/jack_Tomcat.2")
+            JSON3_norm = requests.get(cad_URL1)
+            JSON4_norm = requests.get(cad_URL2)
 
             data_norm_Tom1[i] = JSON3_norm.json()
             data_norm_Tom2[i] = JSON4_norm.json()
@@ -37,8 +35,8 @@ def snap_norm(URL, mean, sd, k):
     if (short==1):
         time.sleep(90.01-(toc-tic))
         i=i+1
-        JSON1_norm = requests.get("http://192.168.99.100:3000/api/v1.3/docker/jack_Tomcat.1")
-        JSON2_norm = requests.get("http://192.168.99.100:3000/api/v1.3/docker/jack_Tomcat.2")
+        JSON1_norm = requests.get(cad_URL1)
+        JSON2_norm = requests.get(cad_URL2)
 
         data_norm_Tom1[i] = JSON1_norm.json()
         data_norm_Tom2[i] = JSON2_norm.json()
@@ -47,8 +45,17 @@ def snap_norm(URL, mean, sd, k):
     data1=list(filter(None, data_norm_Tom1))
     data2=list(filter(None, data_norm_Tom2))
 
+IP = input("Enter Host IP Address  ")
 m = float(input("Enter Mean  "))
 s_d = float(input("Enter Standard Deviation  "))
 n = int(input("Enter the number of times to call the URL  "))
 
-snap_norm("http://192.168.99.100:8080/primecheck",m,s_d,n-1)
+cad_URL1='http://192.168.99.100:3000/api/v1.3/docker/jack_Tomcat.1'
+cad_URL1=cad_URL1[:7] +IP+ cad_URL1[21:]
+cad_URL2='http://192.168.99.100:3000/api/v1.3/docker/jack_Tomcat.2'
+cad_URL2=cad_URL2[:7] +IP+ cad_URL2[21:]
+
+URL="http://192.168.99.100:8080/primecheck"
+URL=URL[:7] +IP+ URL[21:]
+
+snap_normal(URL,m,s_d,n-1)
